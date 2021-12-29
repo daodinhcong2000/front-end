86a011ba7c0e009fd590e3fe1e0fec9b25139a6f
Hank <hank@MacBook-Pro-cua-HANK.local>
2021-12-29 22:39:18 +0700

    update banner, update grid item

--- src/app/home/components/GridItem.jsx
+++ src/app/home/components/GridItem.jsx
@@ -5,7 +5,7 @@ import { Link } from 'react-router-dom'
 
 const GridItem = (props) => {
   console.log(props)
-  const { _id: productId, images = [], name, price, originalPrice, createdAt } = props
+  const { _id: productId, images = [], name, price, originalPrice, createdAt, sold, views } = props
   const created = new Date(createdAt).getTime()
   const isNew = Date.now() - created <= 7 * 24 * 60 * 60 * 1000
   const sale = 100 - Math.floor((price / originalPrice) * 100)
@@ -26,6 +26,42 @@ const GridItem = (props) => {
             </Link>
           </div>
           <figcaption className={`${styles['info-wrap']}`}>
+            <div
+              className={`${styles['title']}`}
+              style={{
+                fontWeight: 'bold',
+                display: '-webkit-box',
+                WebkitLineClamp: 2,
+                WebkitBoxOrient: 'vertical',
+                overflow: 'hidden',
+                textOverflow: 'ellipsis',
+                color: 'black',
+                height: '60px',
+                lineHeight: '30px'
+              }}
+            >
+              {name}
+            </div>
+            <div className={`${styles['row']} ${styles['col-sm-12']}`}>
+              <div className={`${styles['price']} ${styles['mt-1']}`} style={{ color: 'red' }}>
+                ₫ {numberSeparator(price)}
+              </div>
+              {price !== originalPrice && (
+                <div className={`${styles['price-old']} ${styles['mt-2']}`}>
+                  <del>₫ {numberSeparator(originalPrice)}</del>
+                </div>
+              )}
+            </div>
+            <div className={`${styles['row']} ${styles['mt-1']} ${styles['text-dark']}`}>
+              <div className="col-sm-6 text-left">
+                Đã bán <b>{sold}</b>
+              </div>
+              <div className="col-sm-6 text-right">
+                <b>{views}</b> lượt xem
+              </div>
+            </div>
+          </figcaption>
+          {/* <figcaption className={`${styles['info-wrap']}`}>
             <div>
               <div
                 className={`${styles['title']}`}
@@ -56,7 +92,7 @@ const GridItem = (props) => {
                 )}
               </div>
             </div>
-          </figcaption>
+          </figcaption> */}
         </figure>
       </div>
     </>
